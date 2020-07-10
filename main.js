afce39a6e51a93839ad0dabea3b1c151c98e91b4
amandapalma <amandapalma00@gmail.com>
2020-07-10 16:57:44 +0200

    add film image default funtionality

--- main.js
+++ main.js
@@ -7,57 +7,121 @@
 //botón search
 const searchButton = document.querySelector(".js-searchButton");
 
-//campo de búsqueda
-const searchField = document.querySelector(".js-searchField");
-const searchValue = searchField.value;
-// console.log(searchValue);
-// PROBLEMA: NO RECONOCE EL VALOR DEL CAMPO, funciona si le doy un value en el propio input.
-
 //ARRAYS
 
 let films = [];
 let favorites = [];
 
-// Petición al API, para obtener los datos de las películas. Guardamos los datos en la variable 'films', previamente declarada globalmente.
+// (2) Petición al API, para obtener los datos de las películas. Guardamos los datos en la variable 'films', previamente declarada globalmente.
 
 function getDataFromApi() {
+  //campo de búsqueda
+  const searchField = document.querySelector(".js-searchField");
+  const searchValue = searchField.value;
   // console.log(searchValue);
   fetch(`http://api.tvmaze.com/search/shows?q=${searchValue}`)
     .then((response) => response.json())
     .then((data) => {
       films = data;
       paintResults();
-      // console.log(films);
     });
 }
 
+// (3) Función para pintar los resultados de la búsqueda. //???? Y llamar al pintado de estilos de favoritos cuando se cilcka sobre una película
 function paintResults() {
   let codeHTML = "";
+  let favClass;
 
   for (const film of films) {
-    codeHTML += `<li class="js-result-item">`;
-    codeHTML += `<div id= "${film.show.id}" class = "js-result-item-card itemCard" >`;
+    // if (true) {
+    //   favClass = "fav";
+    // } else {
+    //   favClass = "";
+    // }
+    codeHTML += `<li class="js-result-item ${favClass}" id= "${film.show.id}">`;
+    codeHTML += `<div class = "js-result-item-card itemCard" >`;
     codeHTML += `<div class ="itemImg">`;
-    codeHTML += `<img class ="js-result-item-img img" src="${film.show.image.medium}" alt="imagen de ${film.show.name}">`;
+    if (film.show.image !== null) {
+      codeHTML += `<img class ="js-result-item-img img" src="${film.show.image.medium}" alt="alt="${film.show.name} image">`;
+    } else {
+      codeHTML += `<img class ="js-result-item-img img" src="https://via.placeholder.com/210x295/ffffff/666666/?" alt="${film.show.name} image">`;
+    }
     codeHTML += `</div> `;
     codeHTML += `<h4 class="js-result-item-name itemName">${film.show.name}  </h4>`;
     codeHTML += `<i id="js-result-item-heart${film.show.id}" class="far fa-heart itemHeart"></i>`;
     codeHTML += `</div>`;
     codeHTML += `</li>`;
+
+    // for (film of films) {
+    //   if (film.show.id === ) {
+
+    //   }
+
+    // }
   }
   // referencia al ul del HTML
   const resultsList = document.querySelector(".js-results-list-container");
 
-  resultsList.innerHTML += codeHTML;
+  // pintamos en el elemento ul de mi html todo el código que hemos definido arriba y hemos guardado en codeHTML
+  //REVISAR!!! Para eliminar los resultados de la búsqueda anterior y sobreescribir los nuevos
+
+  if ((resultsList.innerHTML = " ")) {
+    resultsList.innerHTML += codeHTML;
+  }
+
+  console.log(resultsList.innerHTML);
+  listenFilmClicks();
 }
 
-// listener para escuchar evento click en el botón search
+//  (1) listener para escuchar evento click en el botón search. Botón search lo hemos definido previamente arriba
 searchButton.addEventListener("click", handleSearch);
 
 // función manejadora de la búsqueda
 function handleSearch(ev) {
   ev.preventDefault();
-  console.log("la función manejadora funciona");
+  console.log("handleSearch funciona");
 
   getDataFromApi();
 }
+
+//----------------------
+//FASE 2: MARCAR COMO FAVORITA
+
+// Cuando se marca una película como favorita:
+// 1) Color de fondo y texto cambian en la lista de resultados
+// 2) Se pinta la película en la lista de favoritos
+// 3) Se guarda esa película en LocalStorage
+
+// Definimos el elemento que queremos escuchar, en este caso la propia tarjeta de la película, y después definimos un listener que recorra cada elemento del array a escuchar, y ejecute la función handleFilmClick
+
+function listenFilmClicks() {
+  const filmItems = document.querySelectorAll(".js-result-item");
+  console.log(filmItems);
+
+  for (const filmItem of filmItems) {
+    filmItem.addEventListener("click", handleFilmClick);
+  }
+  console.log("listenFilmClicks funciona");
+}
+
+function handleFilmClick() {
+  console.log("handleFilmClick funciona");
+}
+
+// Necesitamos identificar la película concreta clickada
+// const handleFilmClick = (ev) => {
+//   //El id de la película es un identificador único, por eso necesitamos obtener ese id.
+//   const clickedId = Number(ev.currentTarget.id);
+//   //Buscamos la película que tenga ese id concreto
+//   const film = films.find((filmItem) => filmItem.id === clickedId);
+//   favorites.push(film);
+//   console.log(favorites);
+// };
+
+// function handleFavorite() {
+//   markAsFavorite();
+//   paintFavorite();
+// }
+
+getDataFromApi();
+paintResults();
